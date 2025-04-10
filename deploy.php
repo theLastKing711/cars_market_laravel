b3d00467262e63b92f8c6690f4fd4a911a24bad3
beshar jeiroudi <lastking711@protonmail.com>
2025-04-10 12:36:05 -0700

    a

--- deploy.php
+++ deploy.php
@@ -15,14 +15,14 @@ add('writable_dirs', []);
 // Hosts
 
 host('68.183.76.49')
-// host('https:\\\digitaldomain.site')
     ->set('remote_user', 'lastking711')
     ->set('deploy_path', '/var/www/html/cars_market_laravel');
-// ->set('remote_user', 'lastking711')
-// ->set('deploy_path', '~/cars_market_laravel');
-
-// tasks
+// ->set('ssh_multiplexing', false)
+// ->set('forward_agent', false)
+// ->set('git_tty', false);
 
 // Hooks
 
 after('deploy:failed', 'deploy:unlock');
+
+https:// www.youtube.com/watch?v=C9_O9dn7SzY
