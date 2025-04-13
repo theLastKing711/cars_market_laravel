5cda289159365f355d6052d5dfb8e9a5d18ec6b2
beshar jeiroudi <lastking711@protonmail.com>
2025-04-13 05:27:25 -0700

    'a'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,37 +14,37 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        Log::info(config('app.env'));
+        // Log::info(config('app.env'));
 
-        if (config('app.env') === 'production') {
+        // if (config('app.env') === 'production') {
 
-            Log::info(config('app.env'));
+        // Log::info(config('app.env'));
 
-            // $roles_count = DB::table('roles')->count();
+        // $roles_count = DB::table('roles')->count();
 
-            // $callable_seeders = [];
+        // $callable_seeders = [];
 
-            // $roles_has_records = $roles_count > 0;
+        // $roles_has_records = $roles_count > 0;
 
-            // if (! $roles_has_records) {
-            //     array_push($callable_seeders, RolesAndPermissionsSeeder::class);
-            // }
+        // if (! $roles_has_records) {
+        //     array_push($callable_seeders, RolesAndPermissionsSeeder::class);
+        // }
 
-            // $subscriptions_count = DB::table('subscriptions')->count();
+        // $subscriptions_count = DB::table('subscriptions')->count();
 
-            // $subscriptions_has_records = $subscriptions_count > 0;
+        // $subscriptions_has_records = $subscriptions_count > 0;
 
-            // if (! $subscriptions_has_records) {
-            //     array_push($callable_seeders, SubscriptionSeeder::class);
-            // }
+        // if (! $subscriptions_has_records) {
+        //     array_push($callable_seeders, SubscriptionSeeder::class);
+        // }
 
-            // if (! empty($callable_seeders)) {
+        // if (! empty($callable_seeders)) {
 
-            //     $this->call($callable_seeders);
-            // }
+        //     $this->call($callable_seeders);
+        // }
 
-            // return;
-        }
+        // return;
+        // }
         // $this->call([
         //     RolesAndPermissionsSeeder::class,
         //     // ManufacturerSeeder::class,
