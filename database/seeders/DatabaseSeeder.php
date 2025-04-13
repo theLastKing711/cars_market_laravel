20305425677fed4e8ff2a24fcfd2ab2e12b458a4
beshar jeiroudi <lastking711@protonmail.com>
2025-04-13 04:45:55 -0700

    added seedes for production

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,11 +12,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        if (env('APP_ENV') === 'production') {
+
+            $roles_count = DB::table('roles')->count();
+
+            $roles_has_records = $roles_count > 0;
+
+            $callable_seeders = [];
+
+            if (! $roles_has_records) {
+                array_push($callable_seeders, RolesAndPermissionsSeeder::class);
+            }
+
+            $subscriptions_count = DB::table('subscriptions')->count();
+
+            $subscriptions_has_records = $subscriptions_count > 0;
+
+            if (! $subscriptions_has_records) {
+                array_push($callable_seeders, SubscriptionSeeder::class);
+            }
+
+            if (! $roles_has_records) {
+
+                $this->call($callable_seeders);
+            }
+
+            return;
+        }
+
         $this->call([
             RolesAndPermissionsSeeder::class,
             // ManufacturerSeeder::class,
             SubscriptionSeeder::class,
-            UserSeeder::class,
+            // UserSeeder::class,
         ]);
     }
 }
