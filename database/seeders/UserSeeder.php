5effca05d99d2bb2dcc6f918b6435581e14fe108
beshar jeiroudi <lastking711@protonmail.com>
2025-04-23 07:06:31 -0700

    'a'

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,102 +17,102 @@ class UserSeeder extends Seeder
             ->staticAdmin()
             ->create();
 
-        // User::factory()
-        //     ->hasRandomSubscriptionType()
-        //     ->has(
-        //         Car::factory()
-        //             ->santaFeFirst()
-        //             ->pick_random_shippable_syrian_cities()
-        //             ->count(1)
-        //     )
-        //     ->create();
+        User::factory()
+            ->hasRandomSubscriptionType()
+            ->has(
+                Car::factory()
+                    ->santaFeFirst()
+                    ->pick_random_shippable_syrian_cities()
+                    ->count(1)
+            )
+            ->create();
 
-        // User::factory()
-        //     ->hasRandomSubscriptionType()
-        //     ->has(
-        //         Car::factory()
-        //             ->santaFeSecond()
-        //             ->pick_random_shippable_syrian_cities()
-        //             ->count(1)
-        //     )
-        //     ->create();
+        User::factory()
+            ->hasRandomSubscriptionType()
+            ->has(
+                Car::factory()
+                    ->santaFeSecond()
+                    ->pick_random_shippable_syrian_cities()
+                    ->count(1)
+            )
+            ->create();
 
-        // User::factory()
-        //     ->hasRandomSubscriptionType()
-        //     ->has(
-        //         Car::factory()
-        //             ->santaFeThrid()
-        //             ->pick_random_shippable_syrian_cities()
-        //             ->count(1)
-        //     )
-        //     ->create();
+        User::factory()
+            ->hasRandomSubscriptionType()
+            ->has(
+                Car::factory()
+                    ->santaFeThrid()
+                    ->pick_random_shippable_syrian_cities()
+                    ->count(1)
+            )
+            ->create();
 
-        // User::factory()
-        //     ->hasRandomSubscriptionType()
-        //     ->has(
-        //         Car::factory()
-        //             ->state([
-        //                 'name_ar' => 'ألانترا',
-        //             ])
-        //             ->pick_random_shippable_syrian_cities()
-        //             ->count(1)
-        //     )
-        //     ->has(
-        //         Car::factory()
-        //             ->state([
-        //                 'name_ar' => 'تويوتا',
-        //                 'name_en' => 'toyota',
-        //             ])
-        //             ->pick_random_shippable_syrian_cities()
-        //             ->count(1)
-        //     )
-        //     ->hasFavouriteCars()
-        //     ->create();
+        User::factory()
+            ->hasRandomSubscriptionType()
+            ->has(
+                Car::factory()
+                    ->state([
+                        'name_ar' => 'ألانترا',
+                    ])
+                    ->pick_random_shippable_syrian_cities()
+                    ->count(1)
+            )
+            ->has(
+                Car::factory()
+                    ->state([
+                        'name_ar' => 'تويوتا',
+                        'name_en' => 'toyota',
+                    ])
+                    ->pick_random_shippable_syrian_cities()
+                    ->count(1)
+            )
+            ->hasFavouriteCars()
+            ->create();
 
-        // User::factory()
-        //     ->hasRandomSubscriptionType()
-        //     ->has(
-        //         Car::factory()
-        //             ->state([
-        //                 'name_ar' => 'كيا ريو',
-        //             ])
-        //             ->count(1)
-        //     )
-        //     ->has(
-        //         Car::factory()
-        //             ->state([
-        //                 'name_ar' => 'تويوتا',
-        //                 'name_en' => 'toyota',
-        //             ])
-        //             ->count(1)
-        //     )
-        //     ->create();
+        User::factory()
+            ->hasRandomSubscriptionType()
+            ->has(
+                Car::factory()
+                    ->state([
+                        'name_ar' => 'كيا ريو',
+                    ])
+                    ->count(1)
+            )
+            ->has(
+                Car::factory()
+                    ->state([
+                        'name_ar' => 'تويوتا',
+                        'name_en' => 'toyota',
+                    ])
+                    ->count(1)
+            )
+            ->create();
 
-        // User::factory()
-        //     ->has(
-        //         Car::factory()
-        //             ->state([
-        //                 'name_ar' => 'النترا',
-        //             ])
-        //             ->count(1)
-        //     )
-        //     ->create();
+        User::factory()
+            ->has(
+                Car::factory()
+                    ->state([
+                        'name_ar' => 'النترا',
+                    ])
+                    ->count(1)
+            )
+            ->create();
 
-        // User::factory()
-        //     ->staticUser()
-        //     ->has(
-        //         Car::factory()
-        //             ->count(15)
-        //     )
-        //     ->create();
+        User::factory()
+            ->staticUser()
+            ->has(
+                Car::factory()
+                    ->count(15)
+            )
+            ->create();
 
-        // User::factory()
-        //     ->syrianNumber()
-        //     ->count(5)
-        //     ->has(
-        //         Car::factory()
-        //             ->count(5)
-        //     )
-        //     ->create();
+        User::factory()
+            ->syrianNumber()
+            ->count(5)
+            ->has(
+                Car::factory()
+                    ->count(5)
+            )
+            ->create();
     }
 }
