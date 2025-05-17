2c25ae28b418358517bb7080f46760cb0519c8c0
beshar jeiroudi <lastking711@protonmail.com>
2025-05-17 14:46:17 -0700

    a

--- routes/console.php
+++ routes/console.php
@@ -1,20 +1,38 @@
 <?php
 
 use App\Models\TemporaryUploadedImages;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::call(function () {
 
-    TemporaryUploadedImages::query()
-        ->whereDate(
-            'created_at',
-            '<',
-            now()
-                ->subtract('+1 day')
-        )
-        ->delete();
+    tap(
+        TemporaryUploadedImages::query()
+            ->whereDate(
+                'created_at',
+                '<',
+                now()
+                    ->subtract('+1 day'),
+            ),
+        function ($query) {
+
+            /** @var Collection<TemporaryUploadedImages> $temporary_images_to_remove */
+            $temporary_images_to_remove =
+                $query
+                    ->get();
+
+            $temporary_images_to_remove
+                ->each(
+                    fn ($item) => Cloudinary::destroy($item->public_id)
+                );
+
+            $query->delete();
+
+        }
+    );
 
 })->everyMinute();
 
