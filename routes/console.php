a2fcc369f8d6eb75fe0b2793581dae6871d3bc09
beshar jeiroudi <lastking711@protonmail.com>
2025-05-17 09:39:47 -0700

    a

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Models\TemporaryUploadedImages;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::call(function () {
+
+    TemporaryUploadedImages::query()
+        ->whereDate(
+            'created_at',
+            '<',
+            now()->subtract('+1 day')
+        );
+
+})->everyMinute();
+
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
